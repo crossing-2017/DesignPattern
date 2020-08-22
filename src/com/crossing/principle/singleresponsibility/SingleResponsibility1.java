a5df638bd8234e9a9e75dee85ee02e512609f3ae
Crossing <crossing_2017@163.com>
2020-08-22 16:56:47 +0800

    first commit

---
+++ src/com/crossing/principle/singleresponsibility/SingleResponsibility1.java
@@ -0,0 +1,13 @@
+package com.crossing.principle.singleresponsibility;
+
+/**
+ * @author Crossing
+ * @Date 2020-08-22
+ */
+public class SingleResponsibility1 {
+
+  public static void main(String[] args) {
+
+  }
+
+}
